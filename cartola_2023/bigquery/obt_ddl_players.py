82eb045e836d5b3c2e8a6fdb6f2914effe058ff1
Gabriel Mizuno <gabrielmizuno@gmail.com>
2023-04-08 17:39:55 -0300

    Separando as OBT

---
+++ cartola_2023/bigquery/obt_ddl_players.py
@@ -0,0 +1,20 @@
+
+from google.cloud import bigquery
+
+from cartola_2023.bigquery.schema import schema_obt_players
+from cartola_2023.constant import ProjectId
+
+client = bigquery.Client('cartola.json')
+
+project_id =  f'{ProjectId.GCP_PROD}'
+dataset_id = 'cartola'
+tabela = 'obt_players'
+table_id = f"{project_id}.{dataset_id}.{tabela}"
+
+dataset_ref = client.dataset(dataset_id, project=project_id)
+
+table_ref = dataset_ref.table(tabela)
+
+table = bigquery.Table(table_ref, schema=schema_obt_players)
+
+table = client.create_table(table)
---
+++ cartola_2023/bigquery/obt_insert_players.py
@@ -0,0 +1,27 @@
+from google.cloud import bigquery
+
+from cartola_2023.bigquery.schema import schema_obt_players
+from cartola_2023.constant import ProjectId
+
+client = bigquery.Client('cartola.json')
+
+project_id =  f'{ProjectId.GCP_PROD}'
+dataset_id = 'cartola'
+tabela = 'obt_players'
+table_id = f"{project_id}.{dataset_id}.{tabela}"
+
+job_config = bigquery.LoadJobConfig(
+    schema=schema_obt_players,
+    source_format=bigquery.SourceFormat.PARQUET,
+    write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
+)
+uri = f"gs://cartola_mizuno/obt/{tabela}/*.parquet"
+
+load_job = client.load_table_from_uri(
+    uri,
+    table_id,
+    project=project_id,
+    job_config=job_config,
+)
+
+load_job.result()
