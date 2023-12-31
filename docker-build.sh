cd05bfcbe13436568a6ec057426ae381f2a4485b
Gabriel Mizuno <gabrielmizuno@gmail.com>
2023-12-31 13:56:37 -0300

    Adicionando Github Actions

--- docker-build.sh
+++ docker-build.sh
@@ -3,6 +3,6 @@
 docker build --platform linux/amd64 -t cartola_local .
 
 docker tag cartola_local local_image
-docker tag cartola_local us-east1-docker.pkg.dev/cartola-360814/cartola-project/cartola-python:0.1.6
+docker tag cartola_local us-east1-docker.pkg.dev/cartola-360814/cartola-project/cartola-python:0.1.11
 
-docker push us-east1-docker.pkg.dev/cartola-360814/cartola-project/cartola-python:0.1.6
+docker push us-east1-docker.pkg.dev/cartola-360814/cartola-project/cartola-python:0.1.11
