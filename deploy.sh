b52bc216a3be2debad0560ea677ecf58d4502d55
Gabriel Mizuno <gabrielmizuno@gmail.com>
2024-01-01 19:36:04 -0300

    Juntando Players e Statistics num mesmo fluxo

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-gcloud run deploy cartola-python \
-        --image=us-east1-docker.pkg.dev/cartola-360814/cartola-project/cartola-python@sha256:dcaf8a92b12d8f6b36cf7d56da1fc8380a7cc8953c637f3a0d922ac091344bf7 \
-        --no-allow-unauthenticated \
-        --port=8080 \
-        --service-account=bucket-cartola@cartola-360814.iam.gserviceaccount.com \
-        --memory=128Mi \
-        --max-instances=1 \
-        --cpu-boost \
-        --region=us-central1 \
-        --project=cartola-360814
+gcloud run jobs create partidas-71 \
+      --image=us-east1-docker.pkg.dev/cartola-360814/cartola-project/cartola-python-matchs-teams:3b1669a8deb6013d771cf72045021777fdaa0d42ed0a771f4a69235a04a67363 \
+      --service-account=cartola-projeto-python@cartola-360814.iam.gserviceaccount.com \
+      --set-env-vars=API_HOST_KEY=1,API_SECERT_KEY=2,LEAGUE_ID=71,SEASON_YEAR=2023 \
+      --region=us-central1 \
+      --project=cartola-360814 \
+      --tasks=1 \
+      --max-retries=2 \
+      --parallelism=1
+
+gcloud scheduler jobs update http partidas-71 \
+  --location us-central1 \
+  --schedule="0 15 * * *" \
+  --uri="https://us-central1-run.googleapis.com/apis/run.googleapis.com/v1/namespaces/cartola-360814/jobs/partidas:run" \
+  --http-method POST \
+#  --oauth-service-account-email cartola-projeto-python@cartola-360814.iam.gserviceaccount.com
+
+gcloud run jobs delete --region us-central1 --quiet partidas-71
