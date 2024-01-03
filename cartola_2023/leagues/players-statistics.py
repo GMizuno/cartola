5fa0c83f8f5d13277b55d6d2fc20149d84e29d4f
Gabriel Mizuno <gabrielmizuno@gmail.com>
2024-01-03 11:54:43 -0300

    Ajustando data e incluindo controle de quantidade de id

--- cartola_2023/leagues/players-statistics.py
+++ cartola_2023/leagues/players-statistics.py
@@ -1,9 +1,6 @@
-from datetime import date
+from datetime import date, timedelta
 
-from cartola_2023.export.players import (
-    export_player_bronze,
-    export_player_silver
-)
+from cartola_2023.export.players import export_player_bronze, export_player_silver
 from cartola_2023.export.statistics import (
     export_statistics_bronze,
     export_statistics_silver,
@@ -19,45 +16,49 @@ from cartola_2023.leagues import (
     season_year,
 )
 
-date_from = date(2023, 4, 1)
-date_to = date(2023, 4, 10)
+date_to = date.today()
+date_from = date_to - timedelta(days=7)
 
 matches_id = filter_by_date(
     gcs, league_id, season_year, date_from, date_to, parquet_reader
 )
 
-result_player = export_player_bronze(
-    api_host_key,
-    api_secert_key,
-    league_id,
-    season_year,
-    matches_id,
-    gcs,
-    parquet_writer,
-)
 
-export_player_silver(
-    result_player,
-    league_id,
-    season_year,
-    gcs,
-    parquet_writer,
-)
+if matches_id != []:
+    result_player = export_player_bronze(
+        api_host_key,
+        api_secert_key,
+        league_id,
+        season_year,
+        matches_id,
+        gcs,
+        parquet_writer,
+    )
 
-result_statistics = export_statistics_bronze(
-    api_host_key,
-    api_secert_key,
-    league_id,
-    season_year,
-    matches_id,
-    gcs,
-    parquet_writer,
-)
+    export_player_silver(
+        result_player,
+        league_id,
+        season_year,
+        gcs,
+        parquet_writer,
+    )
 
-export_statistics_silver(
-    result_statistics,
-    league_id,
-    season_year,
-    gcs,
-    parquet_writer,
-)
+    result_statistics = export_statistics_bronze(
+        api_host_key,
+        api_secert_key,
+        league_id,
+        season_year,
+        matches_id,
+        gcs,
+        parquet_writer,
+    )
+
+    export_statistics_silver(
+        result_statistics,
+        league_id,
+        season_year,
+        gcs,
+        parquet_writer,
+    )
+else:
+    print("No matches found")
